b7e39b858c438882b25c91adfb3b59af1b8de785
heitorfreitasferreira <heitor.ff@ufu.br>
2021-02-25 20:39:18 -0300

    Não era pra ta dando certo, em withdraw e deposit

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -1,10 +1,11 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
-  alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.{Deposit, Withdraw}
 
   # Criando uma faixada para quem for usar a aplicação, ai ao invés de usar Rocketpay.User.Create e os escambal, pra criar um user só usa
   # Rocketpay.create_user()
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
